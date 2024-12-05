0da7d5cc982297848f0aa9f77bcb5a7777d2eb6d
Linus Ungerer <lceungerer@gmail.com>
2024-12-05 18:28:37 +0200

    Playing around with multithreading

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import yfinance as yf
 import pandas as pd
+#from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 ## Define a list of tickers to analyze (currently using a subset of the S&P 500)
-#sp500 = ["AXON","AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "XEL","WELL"] ##Sample data
+#sp500 = ["PLTR","AXON","AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "XEL","WELL"] ##Sample data
+tickers = [
+    'ACIC', 'ADBE', 'ADIL', 'AHT', 'ALKT', 'ALOT', 'ALRM', 'ALT', 'ALTI', 'AMRX',
+    'APPN', 'ARL', 'ATHE', 'ATMU', 'BEN', 'BHLB', 'BNR', 'BRFS', 'BSTZ', 'BTI',
+    'BUSE', 'BV', 'BWMN', 'BZFD', 'CART', 'CATY', 'CCRN', 'CDZIP', 'CFB', 'CHMG',
+    'CMG', 'CSTE', 'CXM', 'CYCN', 'DAKT', 'DDOG', 'DIAX', 'DJTWW', 'DSGN', 'DSGX',
+    'ECX', 'ELA', 'EOI', 'EPAM', 'ESPR', 'EVC', 'EW', 'FIHL', 'FLR', 'FROG', 'FTIIU',
+    'FWONK', 'GDTC', 'GENC', 'GGR', 'GGT', 'GILD', 'GNTY', 'GRPN', 'HEAR', 'HIMX',
+    'HIVE', 'HOFT', 'INDV', 'INOD', 'INTJ', 'IPX', 'JANX', 'LFMD', 'LITE', 'LPTX',
+    'LTRN', 'MCHX', 'METC', 'METCB', 'MFC', 'MRVL', 'NCNO', 'NET', 'NFE', 'NIXX',
+    'NOW', 'NVA', 'NWFL', 'ONIT', 'OOMA', 'PACB', 'PATH', 'PCOR', 'PCRX', 'PD', 'PRO',
+    'PRPL', 'PSTG', 'PW', 'PWOD', 'PYPL', 'QCRH', 'QUAD', 'RDCM', 'RNAC', 'RNG', 'ROKU',
+    'ROP', 'RPD', 'RR', 'SATL', 'SHBI', 'SOND', 'SOUN', 'SPT', 'STCN', 'STOK'
+]
+
 
 #Added in Variance vars
 stock_price_percentage_jump = 1.03
-stock_volume_percentage_jump = 1.2
-candle_percentage_jump=1.03
+stock_volume_percentage_jump = 2 #Adjust this percentage according to how much larger the volume must be.
+candle_percentage_jump = 1.01     #Adjust this percentage according to how large you want the candlestick jump must be from previous close to todays open
+candle_size_percentage_jump = 1.2 #Adjust this percentage according to how much larger the new candle must be than the old one.
 
-end_date = "2024-11-09" #datetime.now()
-start_date ="2024-08-01"
+#end_date = "2024-11-09" #datetime.now()
+#start_date ="2024-01-01"
 
-sp500 = ['MMM', 'AOS', 'ABT', 'ABBV', 'ACN', 'ADBE', 'AMD', 'AES', 'AFL', 'A', 'APD', 'ABNB', 'AKAM', 'ALB', 'ARE', 'ALGN', 'ALLE', 'LNT', 'ALL', 'GOOGL', 'GOOG', 'MO', 'AMZN', 'AMCR', 'AMTM', 'AEE', 'AEP', 'AXP', 'AIG', 'AMT', 'AWK', 'AMP', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'AON', 'APA', 'AAPL', 'AMAT', 'APTV', 'ACGL', 'ADM', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK', 'ADP', 'AZO', 'AVB', 'AVY', 'AXON', 'BKR', 'BALL', 'BAC', 'BAX', 'BDX', 'BRK.B', 'BBY', 'TECH', 'BIIB', 'BLK', 'BX', 'BK', 'BA', 'BKNG', 'BWA', 'BSX', 'BMY', 'AVGO', 'BR', 'BRO', 'BF.B', 'BLDR', 'BG', 'BXP', 'CHRW', 'CDNS', 'CZR', 'CPT', 'CPB', 'COF', 'CAH', 'KMX', 'CCL', 'CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW', 'CE', 'COR', 'CNC', 'CNP', 'CF', 'CRL', 'SCHW', 'CHTR', 'CVX', 'CMG', 'CB', 'CHD', 'CI', 'CINF', 'CTAS', 'CSCO', 'C', 'CFG', 'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CAG', 'COP', 'ED', 'STZ', 'CEG', 'COO', 'CPRT', 'GLW', 'CPAY', 'CTVA', 'CSGP', 'COST', 'CTRA', 'CRWD', 'CCI', 'CSX', 'CMI', 'CVS', 'DHR', 'DRI', 'DVA', 'DAY', 'DECK', 'DE', 'DELL', 'DAL', 'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DHI', 'DTE', 'DUK', 'DD', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'ELV', 'EMR', 'ENPH', 'ETR', 'EOG', 'EPAM', 'EQT', 'EFX', 'EQIX', 'EQR', 'ERIE', 'ESS', 'EL', 'EG', 'EVRG', 'ES', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FDS', 'FICO', 'FAST', 'FRT', 'FDX', 'FIS', 'FITB', 'FSLR', 'FE', 'FI', 'FMC', 'F', 'FTNT', 'FTV', 'FOXA', 'FOX', 'BEN', 'FCX', 'GRMN', 'IT', 'GE', 'GEHC', 'GEV', 'GEN', 'GNRC', 'GD', 'GIS', 'GM', 'GPC', 'GILD', 'GPN', 'GL', 'GDDY', 'GS', 'HAL', 'HIG', 'HAS', 'HCA', 'DOC', 'HSIC', 'HSY', 'HES', 'HPE', 'HLT', 'HOLX', 'HD', 'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUBB', 'HUM', 'HBAN', 'HII', 'IBM', 'IEX', 'IDXX', 'ITW', 'INCY', 'IR', 'PODD', 'INTC', 'ICE', 'IFF', 'IP', 'IPG', 'INTU', 'ISRG', 'IVZ', 'INVH', 'IQV', 'IRM', 'JBHT', 'JBL', 'JKHY', 'J', 'JNJ', 'JCI', 'JPM', 'JNPR', 'K', 'KVUE', 'KDP', 'KEY', 'KEYS', 'KMB', 'KIM', 'KMI', 'KKR', 'KLAC', 'KHC', 'KR', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LDOS', 'LEN', 'LLY', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW', 'LULU', 'LYB', 'MTB', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MTCH', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'META', 'MET', 'MTD', 'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MRNA', 'MHK', 'MOH', 'TAP', 'MDLZ', 'MPWR', 'MNST', 'MCO', 'MS', 'MOS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX', 'NEM', 'NWSA', 'NWS', 'NEE', 'NKE', 'NI', 'NDSN', 'NSC', 'NTRS', 'NOC', 'NCLH', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI', 'ORLY', 'OXY', 'ODFL', 'OMC', 'ON', 'OKE', 'ORCL', 'OTIS', 'PCAR', 'PKG', 'PLTR', 'PANW', 'PARA', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PNR', 'PEP', 'PFE', 'PCG', 'PM', 'PSX', 'PNW', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PEG', 'PTC', 'PSA', 'PHM', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RL', 'RJF', 'RTX', 'O', 'REG', 'REGN', 'RF', 'RSG', 'RMD', 'RVTY', 'ROK', 'ROL', 'ROP', 'ROST', 'RCL', 'SPGI', 'CRM', 'SBAC', 'SLB', 'STX', 'SRE', 'NOW', 'SHW', 'SPG', 'SWKS', 'SJM', 'SW', 'SNA', 'SOLV', 'SO', 'LUV', 'SWK', 'SBUX', 'STT', 'STLD', 'STE', 'SYK', 'SMCI', 'SYF', 'SNPS', 'SYY', 'TMUS', 'TROW', 'TTWO', 'TPR', 'TRGP', 'TGT', 'TEL', 'TDY', 'TFX', 'TER', 'TSLA', 'TXN', 'TPL', 'TXT', 'TMO', 'TJX', 'TSCO', 'TT', 'TDG', 'TRV', 'TRMB', 'TFC', 'TYL', 'TSN', 'USB', 'UBER', 'UDR', 'ULTA', 'UNP', 'UAL', 'UPS', 'URI', 'UNH', 'UHS', 'VLO', 'VTR', 'VLTO', 'VRSN', 'VRSK', 'VZ', 'VRTX', 'VTRS', 'VICI', 'V', 'VST', 'VMC', 'WRB', 'GWW', 'WAB', 'WBA', 'WMT', 'DIS', 'WBD', 'WM', 'WAT', 'WEC', 'WFC', 'WELL', 'WST', 'WDC', 'WY', 'WMB', 'WTW', 'WYNN', 'XEL', 'XYL', 'YUM', 'ZBRA', 'ZBH', 'ZTS']
+
 #cryptoStocks = ['GALAUSD','XRPUSD']
 # Function to analyze stock data for a given ticker
 def analyze_stock(ticker):
     try:
         # Download the last 3 months of daily stock data for the ticker
-        #stock_data = yf.download(ticker, period="3mo", interval="1d" )
+        stock_data = yf.download(ticker, period="3mo", interval="1d" )
 
         #Backtesting to known Breakout. Days must be larger than 3 months range for 50MA
-        stock_data = yf.download(ticker, start=start_date, end=end_date)
+       # stock_data = yf.download(ticker, start=start_date, end=end_date)
         
         # Calculate the 50-day moving average
         stock_data['50_MA'] = stock_data['Close'].rolling(window=50).mean()
@@ -51,6 +68,16 @@ def analyze_stock(ticker):
         if high_volume!=True:
             return None
         
+         # Calculate the candle size for both today and yesterday
+        yesterday_candle_size=yesterday['Close'][ticker]-yesterday['Open'][ticker]
+        latest_candle_size=latest['Close'][ticker]-latest['Open'][ticker]
+
+        # Check if today's candle size is at least 20% larger than yesterday's
+        candlesize_larger=latest_candle_size>= candle_size_percentage_jump*yesterday_candle_size
+        if candlesize_larger!=True:
+            return None
+
+        
         # If both conditions are met, return the stock data
         if price_above_50_ma and high_volume and price_open_is_greater_than_yesterday:
             return {
@@ -70,8 +97,10 @@ def analyze_stock(ticker):
 def generate_watchlist(tickers):
     watchlist = []
     # Loop through each ticker in the list
+    counter = 0
     for ticker in tickers:
-        print(f"Processing {ticker}...")  # Print the ticker being processed
+        counter += 1
+        print(str(counter)+ f" - Processing {ticker}...")  # Print the ticker being processed
         result = analyze_stock(ticker)
         # If the stock meets the breakout criteria, add it to the watchlist
         if result:
@@ -98,7 +127,8 @@ def display_watchlist(watchlist):
         print("-" * 50)  # Add a separator line for better readability
 
 # Generate the watchlist of stocks that meet the breakout criteria
-watchlist = generate_watchlist(sp500)
+#sp500_cleaned = [item for item in sp500 if '^' not in item]
+watchlist = generate_watchlist(tickers)
 
 # Display the results
 display_watchlist(watchlist)
